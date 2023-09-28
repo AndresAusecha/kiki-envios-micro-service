b061267d35eb6a1eafbc9a86c3be42b8cdc9a80a
andresam <andres@aamsis.co>
2023-09-27 20:48:37 -0500

    add rabbit mq to the envio service

--- src/envio/envio.service.ts
+++ src/envio/envio.service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateEnvioDto } from './dto/create-envio.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Envio } from './entities/envio.entity';
 import { Repository } from 'typeorm';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class EnvioService {
   constructor(
     @InjectRepository(Envio) private readonly envioRepository: Repository<Envio>,
+    @Inject('rabbit-mq-module') private readonly client: ClientProxy
   ) {}
 
   create(createEnvioDto: CreateEnvioDto) {
@@ -18,6 +20,7 @@ export class EnvioService {
     envio.estado = createEnvioDto.estado
     envio.fecha_envio = new Date(createEnvioDto.fecha_envio)
     
+    this.client.send("EnvioCreado", { message: "El envío se ha creado correctamente" }).subscribe();
     return this.envioRepository.save(envio);
   }
 
